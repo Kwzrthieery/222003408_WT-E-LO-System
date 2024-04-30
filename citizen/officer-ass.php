b022f5737581faf3a1ec54abb88149b5356c32e1
kwizera thierry <104999793+Kwzrthieery@users.noreply.github.com>
2024-04-30 02:28:51 +0200

    v2.3

--- citizen/officer-ass.php
+++ citizen/officer-ass.php
@@ -19,6 +19,18 @@
             </ul>
         </nav>
     </header>
+    <div class="form-container">
+        <div class="form-box">
+            <h2 class="form-title">View Officer assigned</h2>
+            <form id="response-form" class="response-form">
+                <div class="form-group">
+                    <label for="request-id">Enter Request ID:</label>
+                    <input type="text" id="request-id" name="request-id" required>
+                </div>
+                <button type="submit" class="submit-btn">Submit</button>
+            </form>
+        </div>
+    </div>
 
 
     <button class="logout-btn" onclick="logout()">Logout</button>
